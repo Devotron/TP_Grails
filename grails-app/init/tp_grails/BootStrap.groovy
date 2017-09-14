7960bb9fc009cbd6ec9d80461dd03ce901b66db8
Michaël Gianfaldone <m.gianfaldone@gmail.com>
2017-09-14 12:14:56 +0200

    Ajout du plugin SpringSecurity

--- grails-app/init/tp_grails/BootStrap.groovy
+++ grails-app/init/tp_grails/BootStrap.groovy
@@ -1,8 +1,28 @@
 package tp_grails
 
+import fr.mbds.tpgrails.Role
+import fr.mbds.tpgrails.User
+import fr.mbds.tpgrails.UserRole
+
 class BootStrap {
 
     def init = { servletContext ->
+
+        if (Role.count == 0) {
+            def roleAdmin = new Role(authority: 'ROLE_ADMIN').save(flush: true, failOnError: true)
+            def roleModerateur = new Role(authority: 'ROLE_MODERATEUR').save(flush: true, failOnError: true)
+            def roleUtilisateur = new Role(authority: 'ROLE_UTILISATEUR').save(flush: true, failOnError: true)
+
+            def utilisateur = new User(username: "user", password: "pass").save(flush: true, failOnError: true)
+            def moderateur = new User(username: "modo", password: "passm").save(flush: true, failOnError: true)
+            def administrateur = new User(username: "admin", password: "passa").save(flush: true, failOnError: true)
+
+            UserRole.create(utilisateur, roleUtilisateur, true)
+            UserRole.create(moderateur, roleModerateur, true)
+            UserRole.create(administrateur, roleAdmin, true)
+
+        }
+
     }
     def destroy = {
     }
